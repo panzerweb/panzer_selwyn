6e5b6ea4953008444564f8cf52d77cad39ebe996
PanzerWeb <selwynvillar@gmail.com>
2024-06-11 12:02:15 +0800

    Projects Added

--- app.js
+++ app.js
@@ -28,3 +28,42 @@ window.addEventListener('scroll', function() {
         offHeader.classList.remove('scrolled');
     }
 });
+
+
+// todo Filterable Buttons for each projects
+
+const filterButtons = document.querySelectorAll('.filter-button');
+const projectContainer = document.querySelectorAll('.project-container');
+
+filterButtons.forEach(function(button){
+    button.addEventListener('click', function(){
+        const btnCategory = button.dataset.category;
+
+        projectContainer.forEach(function(container){
+            const containerCategory = container.dataset.category;
+
+            if (btnCategory === 'all' || containerCategory === btnCategory) {
+                container.style.display = 'flex';
+            } else {
+                container.style.display = 'none';
+
+            }
+        })
+    })
+})
+
+// todo Active Button
+// Get the container element
+var btnContainer = document.getElementById("filters");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("filter-button");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
